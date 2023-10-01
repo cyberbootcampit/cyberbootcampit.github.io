810d91d49362c01946a4cf6c1677177a36c03ac7
Leandro Pagano <leandro01@live.it>
2023-10-01 22:41:34 +0200

    Remove js

--- static/js/navbar.js
+++
@@ -1,7 +0,0 @@
-const nav = document.querySelector(".navbar");
-window.onscroll = function () { 
-    if (document.documentElement.scrollTop >= 240)
-        nav.classList.add("scrolled");
-    else
-        nav.classList.remove("scrolled");
-};
